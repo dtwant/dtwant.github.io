b7f07510562ae78eb014d134b109c0610c129a19
dtwant <hiromuskywalker0101@gmail.com>
2026-08-08 02:12:40 +0900

    Site update 2026-08-08 02:12:34

--- js/pwa-config-sync.js
+++ js/pwa-config-sync.js
@@ -26,7 +26,8 @@
     attendance: 'dt_attendance_blob',
     weight_log: 'dt_weight_log_bin_id',
     shift_clock: 'dt_shift_clock_blob',
-    taskmanage: 'dt_taskmanage_blob'
+    taskmanage: 'dt_taskmanage_blob',
+    focus_lab: 'dt_focus_lab_blob'
   };
 
   // デフォルトのマスターAPIキー (公開されてもデータ破壊は起きないよう基本的には個人の読み書き用)
